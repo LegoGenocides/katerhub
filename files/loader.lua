a4ec770f38495f0fabe94922746b03ee13cf96ea
Colin <91840948+Colin4President@users.noreply.github.com>
2026-06-08 22:26:29 +0200

    Add Katerhub v5 loader functionality

--- files/loader.lua
+++ files/loader.lua
@@ -1,1 +1,19 @@
+-- // katerhub v5 loader //
+local katerhubgithub = 'https://raw.githubusercontent.com/LegoGenocides/katerhub/'
+local GetHttp = function(url)
+    local success,response = pcall(function()
+        return game:HttpGet(url)
+    end)
 
+    if success then
+        return response
+    end
+    return nil, response
+end
+
+local Game = GetHttp(katerhubgithub .. 'refs/heads/main/files/addons/'..tostring(game.PlaceId)..'.lua')
+if Game then
+    loadstring(Game)()
+else
+    loadstring(katerhubgithub .. 'refs/heads/main/files/universal.lua')()
+end
